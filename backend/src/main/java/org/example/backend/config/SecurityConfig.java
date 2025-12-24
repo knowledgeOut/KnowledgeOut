09cac14aecccc50b68769a38a3c1129e2fabc0d8
yulb1 <kim482185@gmail.com>
2025-12-24 16:42:18 +0900

    chore: SecurityConfig 추가

---
+++ backend/src/main/java/org/example/backend/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package org.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/knowledgeout/members/signup").permitAll()
+
+                        .anyRequest().authenticated() // 그 외 요청은 인증 필요
+                );
+
+        return http.build();
+    }
+}
