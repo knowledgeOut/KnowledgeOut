8233d13e9e74abdcc7c6b616cc242de6cb127b21
tory <chkoreakr@google.com>
2025-12-24 11:46:57 +0900

    chore: 프로젝트 환경 설정 (gitignore 수정, docker-compose 추가, 불필요한 캐시 삭제)

---
+++ backend/src/main/java/org/example/backend/config/WebConfig.java
@@ -0,0 +1,19 @@
+/*package org.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")                 // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // Next.js 주소 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
+
+ */
