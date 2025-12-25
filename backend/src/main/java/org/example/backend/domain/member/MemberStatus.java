600f274f72ce06acdac8a44073c15ef83536115b
brunstart <brunstart@gmail.com>
2025-12-25 19:23:33 +0900

    remove : answer 관련 파일 삭제
    - answer 엔티티 관련 내용이 dev로 push, 해당 브랜치를 새로 복사해왔기 때문에 불필요한 내용 제거 후 기능 개발에 집중

--- backend/src/main/java/org/example/backend/domain/member/MemberStatus.java
+++
@@ -1,6 +0,0 @@
-package org.example.backend.domain.member;
-
-public enum MemberStatus {
-    ACTIVE,     // 정상 회원
-    DELETED     // 탈퇴 회원
-}
--- backend/src/main/java/org/example/backend/domain/member/Role.java
+++
@@ -1,6 +0,0 @@
-package org.example.backend.domain.member;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
-}
