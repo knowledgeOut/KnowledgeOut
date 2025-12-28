434cc836b37a88481e84145eeaf2a7f4680decd9
brunstart <brunstart@gmail.com>
2025-12-29 05:22:38 +0900

    feat : 질문 전체 조회 추가

--- backend/src/main/java/org/example/backend/controller/QuestionController.java
+++ backend/src/main/java/org/example/backend/controller/QuestionController.java
@@ -3,18 +3,25 @@ package org.example.backend.controller;
 import lombok.RequiredArgsConstructor;
 // import org.example.backend.domain.question.Question;
 import org.example.backend.dto.request.QuestionRequestDto;
+import org.example.backend.dto.response.MemberResponseDTO;
+import org.example.backend.dto.response.QuestionResponseDto;
 import org.example.backend.service.QuestionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/knowledgeout")
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
+
+    @GetMapping("/questions")
+    public ResponseEntity<List<QuestionResponseDto>> getAllQuestions() {
+        List<QuestionResponseDto> questions = questionService.getAllQuestions();
+        return ResponseEntity.ok(questions);
+    }
 
     @PostMapping("/questions")
     public ResponseEntity<Long> addQuestion(@RequestBody QuestionRequestDto questionRequestDto) {
