d55c7f80f103135451a59edb7fe228b9f161e88a
haein <zenq0007@gmail.com>
2026-01-05 20:21:04 +0900

    refactor: 에러 메시지 박스 코드 리팩토링

--- frontend/components/common/AuthDialog.js
+++ frontend/components/common/AuthDialog.js
@@ -323,11 +323,6 @@ export function AuthDialog({
 
           <TabsContent value="signup">
             <form onSubmit={handleSignup} className="space-y-4 pt-4">
-              {signupError && (
-                <div className="bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded text-sm">
-                  {signupError}
-                </div>
-              )}
               <div>
                 <Label htmlFor="signup-nickname">닉네임</Label>
                 <Input
