57673776171a69d9f5575d9bb0d4cbe76ffdd379
brunstart <brunstart@gmail.com>
2026-01-06 14:43:12 +0900

    refactor : tagId 필드명 id로 변경

--- backend/src/main/java/org/example/backend/domain/tag/Tag.java
+++ backend/src/main/java/org/example/backend/domain/tag/Tag.java
@@ -17,7 +17,7 @@ public class Tag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long tagId;
+    private Long id;
 
     @Column(nullable = false, unique = true, length = 50)
     private String name;
